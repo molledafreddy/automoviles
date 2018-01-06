dfb7458823fefb2c3eb9a29049c1939b73fd3457
Freddy Molleda <fmolleda@meritop.com>
2018-01-06 11:40:06 -0400

    primer commit

---
+++ app/User.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App;
+
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class User extends Authenticatable
+{
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'dni','name','birh_date','email','car_model','car_year','password'
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    public function images()
+    {
+        return $this->hasmany(Image::class);
+    }
+}
